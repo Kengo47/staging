cbfd215cfb26f15e37d51d3599843450f48f032d
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-05-10 22:50:56 +0900

    レイアウト周りの改善

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,7 @@ class User < ApplicationRecord
                                     dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_one_attached :avatar
   validates :name, presence: true, length: { maximum: 10 }
 
   devise :database_authenticatable, :registerable,
