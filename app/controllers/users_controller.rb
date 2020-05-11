569e1038f077c6d6a2a4de4b038cb441a469de73
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-05-11 21:51:49 +0900

    レイアウトの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_posts = @user.posts.all
+    @following_users = @user.following.all
+    @followers_users = @user.followers.all
   end
 end
